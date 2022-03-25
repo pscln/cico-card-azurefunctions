f37c4b5e082a3f83bca1886c3df562b7a855527f
pascal <psn754@gmail.com>
2022-03-25 03:15:02 +0100

    add unit tests

--- src/pay/index.js
+++ src/pay/index.js
@@ -1,8 +1,7 @@
 module.exports = async function (context, req) {
     const amount = (req.query.amount || (req.body && req.body.amount));
 
-    // check if amount is a number
-    if(typeof(amount) != 'number' || amount <= 0){
+    if(amount <= 0){
         context.res = {
             status: 400,
             body: {success: false, error: "Illegal amount type! Please provide positive number"}
---
+++ test/unit/pay.test.js
@@ -0,0 +1,4 @@
+test("dummy test", () => {
+    const sum = 1+1;
+    expect(sum).toBe(2);
+})
